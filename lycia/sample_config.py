f0669f3823200e3c1300ab4585b550bd2f86f1e0
こなた <konatadev@bk.ru>
2021-07-05 16:32:18 +0530

    Update sample_config.py

--- lycia/sample_config.py
+++ lycia/sample_config.py
@@ -1,4 +1,4 @@
 class Config(object):
-    API_ID = int("api_id")
-    API_HASH = "api_hash"
-    TOKEN = "token"
+    API_ID = int("3609223")
+    API_HASH = "302bf5498005e091fd23c88d8e76740e"
+    TOKEN = "1603258581:AAHq-tHxPy-DN_kC0yTVukb1YTa4hCjDLAI"
